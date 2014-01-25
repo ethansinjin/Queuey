4389d0e8cd9a689b16a63ca746b1f88429ae9fe3
Jaden Geller <jaden.geller@gmail.com>
2014-01-25 13:38:14 -0800

    Hurry hurry hurry

---
+++ Queuey/EJActionViewController.m
@@ -0,0 +1,38 @@
+//
+//  EJActionViewController.m
+//  Queuey
+//
+//  Created by Jaden Geller on 1/25/14.
+//  Copyright (c) 2014 EJ Dev. All rights reserved.
+//
+
+#import "EJActionViewController.h"
+
+@interface EJActionViewController ()
+
+@end
+
+@implementation EJActionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
